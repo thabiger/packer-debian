e3a91a212bca59f4a89665a77f663237d50dd880
Tomasz Habiger <tomasz.habiger@gmail.com>
2016-08-30 21:03:47 +0200

    Added support for Debian Stretch
    Additional change: the system disk will be partitioned using LVM

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -3,3 +3,8 @@
 apt-get -y install openssh-server
 echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+
+if [ $(facter lsbdistcodename) = 'stretch' ] ; then
+  systemctl disable ssh.service
+  systemctl enable ssh.socket
+fi
